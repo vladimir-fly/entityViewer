5d9a8eb905797c8a67d77e9f4306d9407b18e1e8
VasilchukVV <VasilchukVV@gmail.com>
2016-12-18 05:14:59 +0400

    Add Item & Component prototypes

---
+++ EntityViewer/Models/Component.cs
@@ -0,0 +1,30 @@
+﻿namespace MongoTest
+{
+    public class Component
+    {
+        private Item _item;
+
+        public Component(Item item = null)
+        {
+            _item = item;
+        }
+
+        public Item Item
+        {
+            get { return _item; }
+            set
+            {
+                if (_item == value)
+                    return;
+
+                var item = _item;
+                _item = value;
+                if (item != null)
+                    item.Remove(this);
+
+                if (_item != null)
+                    _item.Add(this);
+            }
+        }
+    }
+}
